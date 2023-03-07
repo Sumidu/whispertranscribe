cf22475aa3408d95eed777ac081f73b6e9672303
André Calero Valdez <andrecalerovaldez@gmail.com>
2023-03-07 20:06:58 +0100

    added some more comments to transcribe.R to make it easire to use.

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 An R based tool that uses Open-AIs whisper to create transcripts from audio.
 
+## Shiny app and R-file
+
+There are two ways how to use whispertranscribe.
+
+### Shiny Application
+There is a shiny application in the `WhisperTranscribe` sub-folder. The app is 
+located in the single `app.R` file. By running this file the shiny app should 
+load.
+
+
+### R-File
+The file `transcribe.R` converts all 
+
+
 See here https://github.com/openai/whisper for troubleshooting.
 
 It needs Python + the Open-AI library.
--- transcribe.R
+++ transcribe.R
@@ -1,27 +1,67 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Default settings ----
+#
+# Change these to your individual requirement.
+#
+# which file-types to convert
+file_types <- ".mp3"
+
+# file prefix for output files
+output_prefix <- "tr_"
+# file extension for output files (raw text)
+output_extension <- ".txt"
+
+# default models to use (can be a vector)
+selected_models <- c("medium")
+
+
+
+
+
+
+
+
+# Start of program ----
+
+# load libraries
 library(tidyverse)
 library(reticulate)
 library(lubridate)
 
-
+# cancel startup if virtual environment does not exist.
+if(!reticulate::virtualenv_exists("r-reticulate")){
+  stop("Please setup the python virtual environment first. See setup.r.")
+}
 use_virtualenv("r-reticulate")
+
+
 # load whisper
 whisper <- import("whisper")
 model_list <- whisper$available_models()
 message(paste("The following models are available:", paste(model_list, collapse = ", ")))
-selected_models <- c("medium")
 
+# load file list
+file_in_list <- dir("input", pattern = file_types)
+message(paste("Converting", length(file_in_list), "files..."))
 
-file_in_list <- dir("input", pattern = ".mp3")
+# _iterate over all files ----
 for(filein in file_in_list){
-  fileout <- paste0("output/tr_", str_sub(filein, 1, -4), ".txt")
+  #generate output name
+  fileout <- paste0("output/", output_prefix, str_sub(filein, 1, -4), output_extension)
   message(paste("Now transcribing file:", filein, "Output file:", fileout))
+  
+  # run all individual models
   for (model_name in selected_models){
     start_time <- Sys.time()
+    
+    # load the model
     model <- whisper$load_model(model_name)
+    # __transcribe the current item ----
     out = model$transcribe(paste0("input/",filein))
     output <- out["text"]$text
     
-    results <- c("test")
+    # convert individual segments with time codes
+    results <- c("")
     for(i in 1:length(out$segments)){
       start <- out$segments[[i]]$start %>% as.period()
       end <- out$segments[[i]]$end %>% as.period()
@@ -31,8 +71,10 @@ for(filein in file_in_list){
     
     write_lines(results, paste0(fileout, "_", model_name, "_bysegments.txt"))
     write_file(out$text, paste0(fileout, "_", model_name, "_full.txt"))
+    
+    # write time to stdout
     end_time <- Sys.time()
-    message(paste("Timing was", as.period(end_time-start_time)))
+    message(paste("Transcription took", as.period(end_time-start_time)))
   }
 }
 
