303389307b4f1e593e10073b7ba14a4916e2f021
André Calero Valdez <andrecalerovaldez@gmail.com>
2023-03-07 16:21:28 +0100

    fixed comments in setup.R

--- setup.R
+++ setup.R
@@ -4,20 +4,23 @@ library(reticulate)
 # create a new environment 
 virtualenv_create("r-reticulate")
 
-# install SciPy
+# install whisper and certifi
 virtualenv_install("r-reticulate", "openai-whisper", pip_options = "-U")
 virtualenv_install("r-reticulate", "certifi", pip_options = "-U")
 
-
+# this activates the library
 use_virtualenv("r-reticulate")
 
+# did it work?
 py_list_packages(envname = "r-reticulate")
 
-# import SciPy (it will be automatically discovered in "r-reticulate")
+# import libraries
 whisper <- reticulate::import("whisper")
 certifi <- reticulate::import("certifi")
 
 
-## SSL Error?
+## Here are some common errors that can happen
+
+## SSL Error on a mac?
 # https://stackoverflow.com/questions/52805115/certificate-verify-failed-unable-to-get-local-issuer-certificate
 ## find install certificates in the python folder.
